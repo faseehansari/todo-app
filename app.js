994406480a6629e3aa2d93b09be2110b5909fe77
faseehansari <faseehahmed009@gmail.com>
2026-03-03 01:07:14 +0500

    add funcility edit & search

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let todo = document.querySelector('li')
 let userInput = document.querySelector('.txt')
 let form =document.querySelector('.input')
 let cmplt = document.querySelector('#chk')
-let edit =document.querySelector('.fa-pen-to-square')
+let searchInput = document.querySelector('.search');
+// let edit =document.querySelector('.fa-pen-to-square')
 // console.log(cmplt);
 
 // console.log(output);
@@ -15,18 +16,24 @@ let edit =document.querySelector('.fa-pen-to-square')
 
 // console.log(dlt);
 
-
+let editIndex = null;
 function add(e){
     e.preventDefault();
 
-    const addition = document.createElement("li");
-    addition.innerHTML = todo.innerHTML;
-    addition.querySelector('#para2').innerText = userInput.value
-    // addition.innerText = "todo.innerText";
-    // addition.textContent = todo.textContent;
-    list.appendChild(addition)
+    if(editIndex === null){
+        const addition = document.createElement("li");
+        addition.innerHTML = todo.innerHTML;
+        addition.querySelector('#para2').innerText = userInput.value
+        // addition.innerText = "todo.innerText";
+        // addition.textContent = todo.textContent;
+        list.appendChild(addition)
+    }else{
+        list.children[editIndex].querySelector('#para2').innerText = userInput.value;
+         editIndex = null;
+         btn.textContent = ` Add Task`;
+    }
 
-    userInput.value = "";
+    form.reset()
     
 }
 
@@ -48,8 +55,39 @@ function taskHandel(e){
             taskText.style.opacity = "1";
         }
     }
+    if(e.target.classList.contains('edit')){
+        const item = list.children;
+        // console.log(item);
+        const target = e.target.parentElement.parentElement;
+        // console.log(target);
+        btn.textContent = 'Update Task'
+        
+        for(let i=0 ;i <item.length; i++ ){
+            if(item[i] === target){
+                  editIndex = i;                
+            }
+        }
+        // userInput.value = e.target.parentElement.parentElement.textContent;
+        userInput.value = target.querySelector('#para2').innerText;
+    }
+
      }
 
 list.addEventListener('click' , taskHandel)
 
+function searchTask(e){
+    let searchText = e.target.value.toLowerCase();
+    let items = list.children;
 
+    for(let i = 1; i < items.length; i++){
+        let taskText = items[i].querySelector('#para2').innerText.toLowerCase();
+
+        if(taskText.includes(searchText)){
+            items[i].style.display = "flex";
+        }else{
+            items[i].style.display = "none";
+        }
+    }
+}
+
+searchInput.addEventListener('keyup', searchTask);
